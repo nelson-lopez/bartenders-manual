079704cd7b8276e3d7c858af7291526274664485
Nelson Lopez <dev.nelsonlopez@gmail.com>
2019-11-11 16:44:58 -0500

    finished initial styling for cocktailist page

---
+++ client/src/api/getCocktails.js
@@ -0,0 +1,9 @@
+// import React,{useState} from 'react';
+// import axios from 'axios'
+
+// const getCocktails = () => {
+//   const [data, setData] = useState(null)
+
+// }
+
+// export default getCocktails;
--- client/src/components/UnauthorizedApp/CocktailList.js
+++ client/src/components/UnauthorizedApp/CocktailList.js
@@ -1,7 +1,38 @@
 import React from 'react';
+import StyledCocktailList from '../component-styles/StyledCocktailList';
 
 const CocktailList = () => {
-  return <div>Cocktaillist</div>;
+  return (
+    <StyledCocktailList>
+      <h2>Cocktails</h2>
+      <div className="container">
+        <div className="cocktail-category">
+          <img src="https://imgur.com/f8zE6QV.png" alt="woops" />
+          <p>Stirred</p>
+        </div>
+        <div className="cocktail-category">
+          <img src="https://imgur.com/kHTje9z.png" alt="woops" />
+          <p>Sour</p>
+        </div>
+        <div className="cocktail-category">
+          <img src="https://imgur.com/u21qIgB.png" alt="woops" />
+          <p>Smash</p>
+        </div>
+        <div className="cocktail-category">
+          <img src="https://imgur.com/kHTje9z.png" alt="woops" />
+          <p>Swizzle</p>
+        </div>
+        <div className="cocktail-category">
+          <img src="https://imgur.com/mosU7WR.png" alt="woops" />
+          <p>Champagne</p>
+        </div>
+        <div className="cocktail-category">
+          <img src="https://imgur.com/1PMUcyd.png" alt="woops" />
+          <p>Dessert</p>
+        </div>
+      </div>
+    </StyledCocktailList>
+  );
 };
 
 export default CocktailList;
