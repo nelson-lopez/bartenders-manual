43c6cf0515c1fdb76d602cf1d776e0805c4d020b
Nelson Lopez <dev.nelsonlopez@gmail.com>
2019-11-11 21:39:20 -0500

    finished rendering individual cocktails/ mvp done

--- client/src/components/UnauthorizedApp/CocktailCard.js
+++ client/src/components/UnauthorizedApp/CocktailCard.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { useState } from "react";
+import useGetCocktailById from "../../api/getCocktailById";
+import StyledCocktail from "../component-styles/StyledCocktail";
 
-const CocktailCard = () => {
-  return <div>test</div>;
+const CocktailCard = props => {
+  const [id, setId] = useState(props.location.state.id);
+
+  const data = useGetCocktailById(id);
+  console.log(data);
+
+  if (data)
+    return (
+      <StyledCocktail>
+        <h2>{data.name}</h2>
+        <img src={data.photo_url} alt="woops" />
+        <p>{data.directions}</p>
+        <p>{data.ingredients}</p>
+      </StyledCocktail>
+    );
+  else return <div>loading</div>;
 };
 
 export default CocktailCard;
---
+++ client/src/components/component-styles/StyledCocktail.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+const StyledCocktail = styled.div`
+  min-width: 81vw;
+  min-height: 100vh;
+  background-color: #0d1635;
+  img {
+    width: 611px;
+    height: 418px;
+  }
+  h2 {
+    width: 252px;
+    height: 113px;
+    font-size: 36px;
+    font-weight: bold;
+    text-align: left;
+    color: #f5efef;
+  }
+`;
+
+export default StyledCocktail;
