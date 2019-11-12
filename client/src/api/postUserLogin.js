4653a38385ee3b940b6aea1df136a0c50edb8b49
Nelson Lopez <dev.nelsonlopez@gmail.com>
2019-11-12 16:39:05 -0500

    grabbing user login input

--- client/src/api/postUserLogin.js
+++ client/src/api/postUserLogin.js
@@ -1,1 +1,27 @@
+import { useState, useEffect } from "react";
 import axios from "axios";
+
+const useUserLogin = (username, password) => {
+  const [token, setToken] = useState(null);
+  const proxy = "https://cors-anywhere.herokuapp.com/";
+  const url =
+    "http://cocktail-db-production.us-east-1.elasticbeanstalk.com/auth/signup";
+
+  useEffect(() => {
+    axios
+      .post(url + proxy, {
+        username: username,
+        password: password
+      })
+      .then(response => {
+        setToken(response);
+      })
+      .catch(error => {
+        throw new Error(error);
+      });
+  }, [username, password]);
+
+  return token;
+};
+
+export default useUserLogin;
--- client/src/components/UnauthorizedApp/Login.js
+++ client/src/components/UnauthorizedApp/Login.js
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import StyledLogin from "../component-styles/StyledLogin";
+import useUserLogin from "../../api/postUserLogin";
 
 const Login = () => {
+  const [input, setInput] = useState({
+    username: "",
+    password: ""
+  });
+
   const handleSubmit = e => {
     e.preventDefault();
   };
+
+  const handleOnInput = e => {
+    const key = e.target.className;
+    const value = e.target.value;
+    setInput(prevState => ({
+      ...prevState,
+      [key]: value
+    }));
+    console.log(e.target.className);
+
+    console.log(input);
+  };
+  // console.log(useUserLogin());
   return (
     <div>
       <StyledLogin>
@@ -12,10 +31,18 @@ const Login = () => {
         <div className="login-container">
           <form onSubmit={handleSubmit}>
             <div>
-              <input placeholder="User Name" className="username"></input>
+              <input
+                placeholder="User Name"
+                className="username"
+                onChange={handleOnInput}
+              ></input>
             </div>
             <div>
-              <input placeholder="Password" className="password"></input>
+              <input
+                placeholder="Password"
+                className="password"
+                onChange={handleOnInput}
+              ></input>
             </div>
             <div>
               <input type="submit" value="Submit" className="submit-button" />
