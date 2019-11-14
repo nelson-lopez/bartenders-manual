b962cd7038b73159598313685a6937bec6aa38a8
Nelson Lopez <dev.nelsonlopez@gmail.com>
2019-11-13 22:28:00 -0500

    scaffolding api call

--- client/src/api/getFavoriteCocktail.js
+++ client/src/api/getFavoriteCocktail.js
@@ -1,24 +1,32 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-const useGetCocktails = type => {
+const useGetFavorites = (token, username) => {
   const url =
-    "http://cocktail-db-production.us-east-1.elasticbeanstalk.com/auth/";
+    "http://cocktail-db-production.us-east-1.elasticbeanstalk.com/auth/find";
   const [data, setData] = useState(null);
 
   console.log(data);
   useEffect(() => {
-    axios
-      .get(url)
-      .then(response => {
-        setData(response.data.filter(cocktail => cocktail.type === type));
-      })
-      .catch(err => {
-        throw new Error(err);
-      });
-  }, [type]);
+    if (token && username) {
+      axios
+        .get(
+          url,
+          {
+            username
+          },
+          {
+            headers: {
+              Authorization: `Bearer ${token}`
+            }
+          }
+        )
+        .then(response => setData(response))
+        .catch(error => new Error(error));
+    }
+  }, [token, username]);
 
   return data;
 };
 
-export default useGetCocktails;
+export default useGetFavorites;
