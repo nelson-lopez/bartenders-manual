dc6de85e2ed05d3e81fa1e5d941e37f02c53dbef
Nelson Lopez <dev.nelsonlopez@gmail.com>
2019-12-28 18:00:02 -0500

    added schema for login and signup

---
+++ client/src/utils/form-utils/SignUpSchema.ts
@@ -0,0 +1,10 @@
+import * as Yup from "yup";
+
+export const SignUpSchema = Yup.object().shape({
+  username: Yup.string()
+    .max(20, "Username is too long!")
+    .min(2, "Username is too short!"),
+  password: Yup.string()
+    .max(20, "Password is too short!")
+    .min(2, "Password is too long")
+});
